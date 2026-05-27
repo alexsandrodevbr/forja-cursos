4a2c7c8ddbaf593ab4e751983d20610671f06a86
Alexsandro <alexsandrosantos47248@gmail.com>
2026-05-27 19:56:59 -0300

    checkout oficial

--- src/App.jsx
+++ src/App.jsx
@@ -529,7 +529,7 @@ export default function App() {
             {/* BOTÃO */}
 
             <a
-              href="https://pay.kiwify.com.br/4QzXScZ"
+              href="https://pay.cakto.com.br/hj6rqht_900869"
               target="_blank"
               className="mt-16 inline-flex items-center justify-center bg-lime-400 text-black font-extrabold text-xl md:text-2xl px-16 py-6 rounded-3xl hover:scale-105 transition-all duration-300 shadow-[0_0_35px_#84cc16]"
             >
